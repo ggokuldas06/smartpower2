b5dcf5da2f9066a65775e31878222f47eb611337
Gokul Das Girish Kumar <gokuldasgirishkumar@Gokuls-MacBook-Air.local>
2025-08-05 12:59:18 +0530

    comp

--- app.py
+++ app.py
@@ -320,3 +320,11 @@ def test_endpoint():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+'''
+    lsof -i :5000
+    flask run --host=0.0.0.0 --port=5000
+
+
+'''
